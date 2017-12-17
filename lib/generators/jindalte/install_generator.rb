25b3f13c02589c8407afd578059fde6ed342d32a
P Kul <1.0@kul.asia>
2017-12-17 05:13:13 -0600

    Fixed fa button

--- lib/generators/jindalte/install_generator.rb
+++ lib/generators/jindalte/install_generator.rb
@@ -7,10 +7,15 @@ module Jindalte
 
     def setup_app
       inside("app/views/layouts") { run "mv application.haml application.haml.bak" }
-      inside("app/assets/javascripts") { run "mv application.js application.js.bak" }
-      inside("app/assets/stylesheets") { run "mv application.scss application.scss.bak" }
+      inside("app/views/layouts") { run "mv lte lte.bak" }
+      inside("app/assets") { run "mv javascripts javascripts.bak" }
+      inside("app/assets") { run "mv stylesheets stylesheets.bak" }
       inside("app/jinda") { run "mv index.mm index.mm.bak"}
+
       directory "app/assets/lte/dist/img"
+      directory "app/views/adminltes"
+      directory "app/views/layouts/lte"
+
     end
 
     def copy_theme
@@ -19,7 +24,7 @@ module Jindalte
       copy_file "application.scss","app/assets/stylesheets/application.scss"
       copy_file "jindalte.css","app/assets/stylesheets/jindalte.css"
       copy_file "application.haml","app/views/layouts/application.haml"
-      copy_file "content-lte.haml","app/views/jinda/content-lte.haml"
+      copy_file "adminltes_controller.rb","app/controllers/adminltes_controller.rb"
       copy_file "index.mm","app/jinda/index.mm"
     end
 
